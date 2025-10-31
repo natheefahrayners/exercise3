1bf16b76676ef2cd785814c93feaeef2919b4c5a
natheefahrayners <natheefahrayners4@gmail.com>
2025-10-31 09:40:25 +0200

    exercise3

---
+++ arrays.js
@@ -0,0 +1,15 @@
+let colors = [];
+colors.push("Red");
+colors.push("Blue");
+colors.push("Green");
+colors.push("Yellow");
+colors.push("Black");
+
+let numbers = [5, 10, 15, 20];
+
+colors.pop();
+
+colors.unshift("White");
+
+console.log(`Number of colors: ${colors.length}`);
+console.log(`Number of numbers: ${numbers.length}`);
